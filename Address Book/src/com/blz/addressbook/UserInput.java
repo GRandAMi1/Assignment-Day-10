f8d2aea5b22dd18e84ababd1929048dfbf665669
GRandAMi1 <Amitshendge1990@gmail.com>
2021-08-07 16:58:55 +0530

    Added multiple User details input and store.

---
+++ Address Book/src/com/blz/addressbook/UserInput.java
@@ -0,0 +1,16 @@
+package com.blz.addressbook;
+
+import java.util.Scanner;
+
+public class UserInput {
+    private static final Scanner SC = new Scanner(System.in);
+
+    public static int intInput() {
+        return SC.nextInt();
+    }
+
+    public static String stringInput() {
+        return SC.next();
+    }
+}
+
